5e443295434a95d0e09bdcefc3e46d8ccf51706e
hechavar@gmail.com <hechavar@gmail.com@d7357073-c146-f551-594e-4a614c68dcd7>
2013-11-30 01:37:38 +0000

    Agrego mensajes para que los puedan probar uds.

--- materials-layer/src/main/java/ClientePruebaContraIntegracion.java
+++ materials-layer/src/main/java/ClientePruebaContraIntegracion.java
@@ -22,12 +22,17 @@ public class ClientePruebaContraIntegracion {
 	    	GuardarDatos guardar = new GuardarDatos();
 	    	guardar.setXml("<WS><hola><id>IdAmbiente</id><id>IdRecurso</id></hola></WS>");
 	    	GuardarDatosResponse g_resp = stub.guardarDatos(guardar);
+	    	
+	    	System.out.println("Respuesta recibida de Integracion:");
 	    	System.out.println(g_resp.get_return());
+	    	System.out.println("Todo anda OK!");
+	    	
 		} catch (AxisFault e) {
 			e.printStackTrace();
+			System.out.println("Hay errores de AxisFault.. tal vez falta una libreria.. avisa a HUGO ");
 		} catch (RemoteException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			System.out.println("Hay errores de RemoteException.. levantaste el servicio? ");
 		}
 
 
