4808515679e9b1302d75121affde7e498c814425
hechavar@gmail.com <hechavar@gmail.com@d7357073-c146-f551-594e-4a614c68dcd7>
2013-12-12 15:44:28 +0000

    Agrego clase Cache

---
+++ materials-layer/src/main/java/connection/cache/Cache.java
@@ -0,0 +1,37 @@
+package connection.cache;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Cache<T> {
+
+	public static int MAX_ITEMS_LIST = 10;
+	private Deque<T> elements;
+
+	public Cache() {
+		elements = new ArrayDeque<T>(MAX_ITEMS_LIST);
+	}
+
+	public void add(T element) {
+		if (elements.offerLast(element)) {
+			elements.remove();
+			elements.offerLast(element);
+		}
+	}
+
+	public boolean contains(T element) {
+		return elements.contains(element);
+	}
+
+	public T get(T element) {
+		if (contains(element)) {
+			for (T cached : elements) {
+				if (cached.equals(element)) {
+					return cached;
+				}
+			}
+		}
+		return null;
+	}
+
+}
---
+++ materials-layer/src/test/java/connection/EncuestaRequesterTest.java
@@ -0,0 +1,14 @@
+package connection;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EncuestaRequesterTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
