3804d643193fa19ffb49775d4df8f690fde9b409
hechavar@gmail.com <hechavar@gmail.com@d7357073-c146-f551-594e-4a614c68dcd7>
2013-12-16 04:19:18 +0000

    codigo de prueba (comentado) para el setArchivo.

--- materials-layer/src/main/java/model/Archivo.java
+++ materials-layer/src/main/java/model/Archivo.java
@@ -1,30 +1,35 @@
 package model;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import javax.activation.DataHandler;
 
 public class Archivo extends Recurso {
 
 	public Archivo(Integer idRecurso, Integer idAmbiente, String descripcion) {
 		super(idRecurso, idAmbiente, descripcion);
-		this.tipo="Archivo";
+		this.tipo = "Archivo";
 		// TODO Auto-generated constructor stub
 	}
 
 	public Archivo() {
-		this.tipo="Archivo";
+		this.tipo = "Archivo";
 	}
 
-//	private String path;
+	// private String path;
 
 	private Integer size;
 
 	private String tipoArchivo;
 
 	private String nombreArchivo;
-	
+
 	private DataHandler rawFile;
-	
-    public String getNombreArchivo() {
+
+	public String getNombreArchivo() {
 		return nombreArchivo;
 	}
 
@@ -33,20 +38,36 @@ public class Archivo extends Recurso {
 	}
 
 	public String getTipoArchivo() {
-        return tipoArchivo;
-    }
- 
-    public void setTipoArchivo(String fileType) {
-        this.tipoArchivo = fileType;
-    }
- 
-    public DataHandler getFile() {
-        return rawFile;
-    }
- 
-    public void setRawFile(DataHandler file) {
-        this.rawFile = file;
-    }
+		return tipoArchivo;
+	}
+
+	public void setTipoArchivo(String fileType) {
+		this.tipoArchivo = fileType;
+	}
+
+	public DataHandler getFile() {
+		return rawFile;
+	}
+
+	public void setRawFile(DataHandler file) {
+		this.rawFile = file;
+
+		// pruebo grabar el archivo recibido
+		// TODO: solo para pruebas.. dejar comentado
+
+//		try {
+//			File tempFile = File.createTempFile("123probando", ".txt");
+//			FileOutputStream fos = new FileOutputStream(tempFile);
+//			rawFile.writeTo(fos);
+//			fos.flush();
+//			fos.close();
+//			System.out.println("Saved response to file : " + tempFile.getAbsolutePath());
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+	}
 
 	public Integer getSize() {
 		return size;
