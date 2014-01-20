5dba449a661b8a10f21db452705c037753274b80
hechavar@gmail.com <hechavar@gmail.com@d7357073-c146-f551-594e-4a614c68dcd7>
2014-01-20 06:14:11 +0000

    guardar archivo

--- materials-layer/src/main/java/connection/ArchivoRequester.java
+++ materials-layer/src/main/java/connection/ArchivoRequester.java
@@ -38,25 +38,25 @@ public class ArchivoRequester {
 
 	public void save(Archivo archivo) {
 		String archivo_str = parser.serializeArchivo(archivo);
-		//IntegracionStub.GuardarArchivoResponse responseArchivo = null;
-	//	try {
+		IntegracionStub.GuardarArchivoResponse responseArchivo = null;
+		try {
 			
-		//	requestArchivo = new IntegracionStub.GuardarArchivo();
+			IntegracionStub.GuardarArchivo requestArchivo = new IntegracionStub.GuardarArchivo();
 			
-		//OP1	requestArchivo.setArchivo(archivo.getByteArray()); asi me dijeron por mail
-		//OP2	requestArchivo.setArchivo(archivo.getFile()); asi lo hacen por el ejemplo q me mandaron
-		//	requestArchivo.setXml(archivo_str);
+//			requestArchivo.setArchivo(archivo.getByteArray()); 
+			requestArchivo.setArchivo(archivo.getRawFile()); 
+			requestArchivo.setXml(archivo_str);
 			
-		//	responseArchivo = this.stub.guardarArchivo(requestArchivo);
-		//	System.out.println("La salida es : " + responseArchivo.get_return());		
-	//	} catch (AxisFault e) {
+			responseArchivo = this.stub.guardarArchivo(requestArchivo);
+			System.out.println("La salida es : " + responseArchivo.get_return());		
+		} catch (AxisFault e) {
 			// e.printStackTrace();
 			System.out.println("Error al intentar guardar la siguiente Archivo:");
 			System.out.println(archivo.getDescripcion());
-	//	} catch (RemoteException e) {
-			// e.printStackTrace();
+		} catch (RemoteException e) {
+			 e.printStackTrace();
 			System.out.println("Error de conexion remota");
-	//	}
+		}
 	}
 	public Archivo getArchivo(int idAmbiente, int idRecurso){
 		Archivo adevolver = new Archivo();
