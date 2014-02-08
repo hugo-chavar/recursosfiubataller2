f3dff8a438b27ecc560c082df8afd1db4238ceaa
hechavar@gmail.com <hechavar@gmail.com@d7357073-c146-f551-594e-4a614c68dcd7>
2014-02-08 18:35:40 +0000

    ClientePruebaContraIntegracion mejorado

--- materials-layer/src/main/java/ClientePruebaContraIntegracion.java
+++ materials-layer/src/main/java/ClientePruebaContraIntegracion.java
@@ -3,93 +3,64 @@ import connection.exceptions.ConnectionException;
 
 public class ClientePruebaContraIntegracion {
 
+	private static IntegracionProxy ip = new IntegracionProxy();
+	private static String xml;
+
 	public static void main(String[] args) {
 
-		IntegracionProxy ip = new IntegracionProxy();
-		String xml;
-		
-//		try {
-//			xml = ip.guardar("<WS><Link><id>400</id><nombre>www.hola.com</nombre></Link></WS>");
-//			System.out.println(xml);
-//		} catch (ConnectionException e1) {
-//			System.out.println(e1.getMessage());
-//		}
-//
-//		try {
-//			xml = ip.seleccionar("<WS><Usuario><username>javier</username></Usuario></WS>");
-//			System.out.println(xml);
-//		} catch (ConnectionException e) {
-//			System.out.println(e.getMessage());
-//		}
-//		try {
-//			xml = ip.seleccionar("<WS><Usuario><id>117</id></Usuario></WS>");
-//			System.out.println(xml);
-//		} catch (ConnectionException e) {
-//			System.out.println(e.getMessage());
-//		}
-//		try {
-//			xml = ip.seleccionar("<WS><Link><id>1002</id></Link></WS>");
-//			System.out.println(xml);
-//		} catch (ConnectionException e) {
-//			System.out.println(e.getMessage());
-//		}
-//
-//		try {
-//			xml = ip.seleccionar("<WS><Recurso><id>997</id></Recurso></WS>");
-//			System.out.println(xml);
-//		} catch (ConnectionException e) {
-//			System.out.println(e.getMessage());
-//		}
-		try {
-			xml = ip.seleccionar("<WS><Recurso><ambitoId>1</ambitoId></Recurso></WS>");
-			System.out.println(xml);
-		} catch (ConnectionException e) {
-			System.out.println(e.getMessage());
-		}
-//		try {
-//			xml = ip.seleccionar("<WS><Link><recursoId>997</recursoId></Link></WS>");
-//			System.out.println(xml);
-//		} catch (ConnectionException e) {
-//			System.out.println(e.getMessage());
-//		}
-//		try {
-//			xml = ip.seleccionar("<WS><Encuesta><id>1003</id></Encuesta></WS>");
-//			System.out.println(xml);
-//		} catch (ConnectionException e) {
-//			System.out.println(e.getMessage());
-//		}
-		try {
-			xml = ip.seleccionar("<WS><EncuestaRespondida><id>1003</id></EncuestaRespondida></WS>");
-			System.out.println(xml);
-		} catch (ConnectionException e) {
-			System.out.println(e.getMessage());
-		}
-//		try {
-//			xml = ip.seleccionar("<WS><Recurso><descripcion>prueba</descripcion></Recurso></WS>");
-//			System.out.println(xml);
-//		} catch (ConnectionException e) {
-//			System.out.println(e.getMessage());
-//		}
-//		try {
-//			xml = ip.guardar("<WS><Recurso><descripcion>prueba</descripcion><tipo>L</tipo></Recurso></WS>");
-//			System.out.println(xml);
-//		} catch (ConnectionException e) {
-//			System.out.println(e.getMessage());
-//		}
-//		try {
-//			xml = ip.guardar("<?xml version=\"1.0\"?><WS><Link><nombre>prueba</nombre></Link></WS>");
-//			System.out.println(xml);
-//		} catch (ConnectionException e) {
-//			System.out.println(e.getMessage());
-//		}
-		
-//		try {
-//			xml = ip.eliminar("<WS><Recurso><id>1006</id></Recurso></WS>");
-//			System.out.println(xml);
-//		} catch (ConnectionException e) {
-//			System.out.println(e.getMessage());
-//		}
+		seleccionar("<WS><Usuario><username>javier</username></Usuario></WS>");
 
+		seleccionar("<WS><Usuario><id>117</id></Usuario></WS>");
+
+		seleccionar("<WS><Link><id>1002</id></Link></WS>");
+
+		seleccionar("<WS><Recurso><id>997</id></Recurso></WS>");
+
+		seleccionar("<WS><Recurso><ambitoId>1</ambitoId></Recurso></WS>");
+
+		seleccionar("<WS><Link><recursoId>997</recursoId></Link></WS>");
+
+		seleccionar("<WS><Encuesta><id>1003</id></Encuesta></WS>");
+
+		seleccionar("<WS><EncuestaRespondida><id>1003</id></EncuestaRespondida></WS>");
+
+		seleccionar("<WS><Recurso><descripcion>prueba</descripcion></Recurso></WS>");
+
+		guardar("<WS><Link><id>400</id><nombre>www.hola.com</nombre></Link></WS>");
+
+		guardar("<WS><Recurso><descripcion>prueba</descripcion><tipo>L</tipo></Recurso></WS>");
+
+		guardar("<?xml version=\"1.0\"?><WS><Link><nombre>prueba</nombre></Link></WS>");
+
+		eliminar("<WS><Recurso><id>1006</id></Recurso></WS>");
+
+	}
+
+	private static void eliminar(String xmlInput) {
+		try {
+			xml = ip.eliminar(xmlInput);
+			System.out.println(xml);
+		} catch (ConnectionException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	private static void guardar(String xmlInput) {
+		try {
+			xml = ip.guardar(xmlInput);
+			System.out.println(xml);
+		} catch (ConnectionException e1) {
+			System.out.println(e1.getMessage());
+		}
+	}
+
+	private static void seleccionar(String xmlInput) {
+		try {
+			xml = ip.seleccionar(xmlInput);
+			System.out.println(xml);
+		} catch (ConnectionException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 }
