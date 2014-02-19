1c615b918cfb835e61f621c285aa827feced5ddd
damianmanoff@gmail.com <damianmanoff@gmail.com@d7357073-c146-f551-594e-4a614c68dcd7>
2014-02-19 17:59:54 +0000

    Dami: Pruebas contra Integración

--- materials-layer/src/main/java/ClientePruebaContraIntegracion.java
+++ materials-layer/src/main/java/ClientePruebaContraIntegracion.java
@@ -26,7 +26,7 @@ public class ClientePruebaContraIntegracion {
 //
 //		seleccionar("<WS><Link><recursoId>997</recursoId></Link></WS>");
 //
-		seleccionar("<WS><Encuesta><recursoId>1003</recursoId></Encuesta></WS>");
+//		seleccionar("<WS><Encuesta><recursoId>1003</recursoId></Encuesta></WS>");
 //
 //		seleccionar("<WS><EncuestaRespondida><recursoId>1003</recursoId><usuarioId>19</usuarioId></EncuestaRespondida></WS>");
 		
@@ -45,54 +45,60 @@ public class ClientePruebaContraIntegracion {
 //		guardar("<WS><Encuesta><recursoId>1030</recursoId><evaluada>false</evaluada><preguntas>F;1;de que color es el caballo blanco de san martin?;rojo,verde,azul,blanco;4|F;2;a que equipo del futbol argentino le denominan Millo;velez,River Plate,crucero del norte,estudiantes;2|F;3;cual es un patron de diseno creacional;command,mediator,builder,facade;3|F;4;Un test unitario debe presentar las siguientes características;Rapido,Moldeable,Configurable,Acoplable,Lento,Extensible,Repetible,Profesional,Maduro,Amplio,Simple,Independiente,Automatizable;1,8,11,12,13,7|C;5;cuantas patas tiene un gato?;4</preguntas></Encuesta></WS>");
 		
 //		guardar("<WS><Encuesta><recursoId>1031</recursoId><evaluada>false</evaluada><preguntas>abc</preguntas></Encuesta></WS>");
-//		String path, nombre, extension;
-//		nombre = "teofilo";
-//		extension = "jpg";
-//		path = ClientePruebaContraIntegracion.class.getProtectionDomain().getCodeSource().getLocation().toExternalForm();
-//		path = path.substring(0, path.lastIndexOf("classes") + 8);
-//		path = path + nombre + "." + extension;
-//		xml = "<WS><ArchivoMetadata><nombre>teofilo2</nombre><tamanio>1</tamanio><tipo>jpg</tipo><recursoId>1018</recursoId></ArchivoMetadata></WS>";
-////		xml = "<WS><ArchivoMetadata><nombre>teofilo</nombre><recursoId>1012</recursoId></ArchivoMetadata></WS>";
-////		xml = "<WS><ArchivoMetadata><nombre>teofilo</nombre><id>1018</id></ArchivoMetadata></WS>";
-////		guardarArchivo(xml, path);
+		String path, nombre, extension;
+		nombre = "hola";
+		extension = "txt";
+		guardar("<WS><Recurso><descripcion>Archivo chico Prueba </descripcion><tipo>A</tipo></Recurso></WS>");
+		path = ClientePruebaContraIntegracion.class.getProtectionDomain().getCodeSource().getLocation().toExternalForm();
+		path = path.substring(0, path.lastIndexOf("classes") + 8);
+		path = path + nombre + "." + extension;
+		
+		System.out.println(path);
+		path = "file:/home/damian/hola.txt";
+		xml = "<WS><ArchivoMetadata><nombre>hola</nombre><tamanio>14</tamanio><tipo>txt</tipo><recursoId>1036</recursoId></ArchivoMetadata></WS>";
+//		xml = "<WS><ArchivoMetadata><nombre>teofilo</nombre><recursoId>1012</recursoId></ArchivoMetadata></WS>";
+//		xml = "<WS><ArchivoMetadata><nombre>teofilo</nombre><id>1018</id></ArchivoMetadata></WS>";
+		guardarArchivo(xml, path);
 //		
 //		PruebasDeArchivo();
 		
 	}
 
-//	private static void PruebasDeArchivo(){
-//		String path, nombre, extension;
-//		
-//		System.out.println("COMIENZA TESTEO DE ARCHIVOS");
-//		nombre = "teofilo";
-//		extension = "jpg";
-//		path = ClientePruebaContraIntegracion.class.getProtectionDomain().getCodeSource().getLocation().toExternalForm();
-//		path = path.substring(0, path.lastIndexOf("classes") + 8);
-//		path = path + nombre + "." + extension;
-//		
-////		xml = "<?xml version=\"1.0\"?><WS><ArchivoMetadata><nombre>teofilo2</nombre><tamanio>1</tamanio><tipo>jpg</tipo><recursoId>1012</recursoId></ArchivoMetadata></WS>";
-//		xml = "<WS><ArchivoMetadata><recursoId>1012</recursoId></ArchivoMetadata></WS>";
-////		xml = "<WS><ArchivoMetadata><nombre>teofilo</nombre><recursoId>1012</recursoId></ArchivoMetadata></WS>";
-////		xml = "<WS><ArchivoMetadata><nombre>teofilo</nombre><id>1012</id></ArchivoMetadata></WS>";
-////		System.out.println("Va a guardar un archivo");
-//		System.out.println("TEST 1 TRAER ARCHIVO EXISTENTE");
-//		try {
-////			xml = "<?xml version=\"1.0\"?><WS><ArchivoMetadata><recursoId>1012</recursoId></ArchivoMetadata></WS>";
-//			com.ws.services.IntegracionStub.ReturnedObject[] objects = ip.seleccionarArchivo(xml);
-//			if(objects==null){
-//				System.out.println("La consulta no trajo resultados");
-//				
-//			}else{
-//				System.out.println("Algo Trajo");
-//				System.out.println("Cantidad: " + objects.length);
-//				com.ws.services.IntegracionStub.ReturnedObject supuestoArchivo = objects[0];
-////				System.out.println(supuestoArchivo.getId());
-//			}
-//		} catch (ConnectionException e) {
-//			System.out.println("No lo pudo traer al archivo");
-//			//e.printStackTrace();
-//		}
-//	}
+	private static void PruebasDeArchivo(){
+		String path, nombre, extension;
+		
+		System.out.println("COMIENZA TESTEO DE ARCHIVOS");
+		nombre = "teofilo";
+		extension = "jpg";
+		path = ClientePruebaContraIntegracion.class.getProtectionDomain().getCodeSource().getLocation().toExternalForm();
+		path = path.substring(0, path.lastIndexOf("classes") + 8);
+		path = path + nombre + "." + extension;
+	
+		path = "/home/damian/hola.txt";
+//		xml = "<?xml version=\"1.0\"?><WS><ArchivoMetadata><nombre>teofilo2</nombre><tamanio>1</tamanio><tipo>jpg</tipo><recursoId>1012</recursoId></ArchivoMetadata></WS>";
+		xml = "<WS><ArchivoMetadata><recursoId>1034</recursoId></ArchivoMetadata></WS>";
+//		xml = "<WS><ArchivoMetadata><nombre>teofilo</nombre><recursoId>1012</recursoId></ArchivoMetadata></WS>";
+//		xml = "<WS><ArchivoMetadata><nombre>teofilo</nombre><id>1012</id></ArchivoMetadata></WS>";
+//		System.out.println("Va a guardar un archivo");
+		System.out.println("TEST 1 TRAER ARCHIVO EXISTENTE");
+		try {
+			xml = "<?xml version=\"1.0\"?><WS><ArchivoMetadata><recursoId>1034</recursoId></ArchivoMetadata></WS>";
+			com.ws.services.IntegracionStub.ArchivoMetadata[] objects = ip.seleccionarArchivo(xml);
+			if(objects==null){
+				System.out.println("La consulta no trajo resultados");
+				
+			}else{
+				System.out.println("Algo Trajo");
+				System.out.println("Cantidad: " + objects.length);
+				com.ws.services.IntegracionStub.ArchivoMetadata supuestoArchivo = objects[0];
+				
+				System.out.println("Nombre de lo que trajo: "+supuestoArchivo.getNombre());
+			}
+		} catch (ConnectionException e) {
+			System.out.println("No lo pudo traer al archivo");
+			//e.printStackTrace();
+		}
+	}
 	
 	private static void eliminar(String xmlInput) {
 		System.out.println("Eliminando " + xmlInput.substring(0, Math.min(xmlInput.length(), 45)) + "..");
